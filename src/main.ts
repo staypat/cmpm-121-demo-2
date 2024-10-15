fa6f79084ace05da627f477cd6e7aee74b22fbd7
Patrick Hu <phhu@ucsc.edu>
2024-10-15 13:26:43 -0700

    Step 1 Completed: Added Canvas UI

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,12 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const header = document.createElement("h1");
+header.innerHTML = APP_NAME;
+app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.append(canvas);
