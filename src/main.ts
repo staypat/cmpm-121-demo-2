8970c9917b2ddbc6c639dddb4bf40a02a9e5db8e
Patrick Hu <phhu@ucsc.edu>
2024-10-15 20:38:51 -0700

    Step 3 Completed - Display list and observer

--- src/main.ts
+++ src/main.ts
@@ -17,32 +17,59 @@ app.append(canvas);
 
 const ctx = canvas.getContext("2d")!;
 
+const lines: { x: number; y: number; }[][] = [];
+
+let currentLine: { x: number; y: number; }[] | null = null;
+
 const cursor = {active: false, x: 0, y: 0};
 
 canvas.addEventListener("mousedown", (event) =>{
     cursor.active = true;
     cursor.x = event.offsetX;
     cursor.y = event.offsetY;
+
+    currentLine = [];
+    lines.push(currentLine);
+    currentLine.push({x: cursor.x, y: cursor.y});
 })
 
 canvas.addEventListener("mousemove", (event) =>{
-    if(cursor.active){
-        ctx.beginPath();
-        ctx.moveTo(cursor.x, cursor.y);
-        ctx.lineTo(event.offsetX, event.offsetY);
-        ctx.stroke();
+    if(cursor.active && currentLine){
         cursor.x = event.offsetX;
         cursor.y = event.offsetY;
+        currentLine.push({ x: cursor.x, y: cursor.y });
+        canvas.dispatchEvent(new Event("drawing-changed"));
     }
 })
 
 canvas.addEventListener("mouseup", () =>{
     cursor.active = false;
+    currentLine = null;
 })
+
+canvas.addEventListener("drawing-changed", () => {
+    redraw();
+});
+
+function redraw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    lines.forEach(line => {
+        if(line.length > 1){
+            ctx.beginPath();
+            const {x, y} = line[0];
+            ctx.moveTo(x, y);
+            for(const {x, y} of line){
+                ctx.lineTo(x, y);
+            }
+            ctx.stroke();
+        }
+    });
+}
 
 const clearButton = document.createElement("button");
 clearButton.innerHTML = "Clear";
 app.append(clearButton);
 clearButton.addEventListener("click", () =>{
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    lines.length = 0;
 })
