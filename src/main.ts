e7ce8032900507cc51f029ebc1bb5be8a096cb3d
Patrick Hu <phhu@ucsc.edu>
2024-10-15 19:09:38 -0700

    Step 2 Completed - Basic drawing/clear function added

--- src/main.ts
+++ src/main.ts
@@ -14,3 +14,35 @@ const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 app.append(canvas);
+
+const ctx = canvas.getContext("2d")!;
+
+const cursor = {active: false, x: 0, y: 0};
+
+canvas.addEventListener("mousedown", (event) =>{
+    cursor.active = true;
+    cursor.x = event.offsetX;
+    cursor.y = event.offsetY;
+})
+
+canvas.addEventListener("mousemove", (event) =>{
+    if(cursor.active){
+        ctx.beginPath();
+        ctx.moveTo(cursor.x, cursor.y);
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        cursor.x = event.offsetX;
+        cursor.y = event.offsetY;
+    }
+})
+
+canvas.addEventListener("mouseup", () =>{
+    cursor.active = false;
+})
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+app.append(clearButton);
+clearButton.addEventListener("click", () =>{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
